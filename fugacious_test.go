37db12e11d857609ff0488cc22cfd552dd86307b
Joshua Carp <jm.carp@gmail.com>
2016-12-07 10:42:50 -0500

    Update expected fugacious response code.

--- fugacious_test.go
+++ fugacious_test.go
@@ -29,7 +29,7 @@ var _ = Describe("Fugacious", func() {
 
 	Context("when the status code is expected", func() {
 		BeforeEach(func() {
-			header = http.StatusFound
+			header = http.StatusCreated
 		})
 
 		It("retrieves a link from the fugacious server", func() {
